829c7d8b59ce04a84b639a6c51d67ffe9e74491f
Brian Martinez <100006298+bamart-dev@users.noreply.github.com>
2025-05-22 19:55:04 -0700

    implement drawLetters; ran wave 1 test suite (all tests passed)

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,5 +1,36 @@
+const LETTER_DISTRIBUTION = {
+    'A': 9, 'B': 2, 'C': 2, 'D': 4, 'E': 12, 'F': 2, 'G': 3, 'H': 2, 'I': 9,
+    'J': 1, 'K': 1, 'L': 4, 'M': 2, 'N': 6, 'O': 8, 'P': 2, 'Q': 1, 'R': 6,
+    'S': 6, 'T': 6, 'U': 4, 'V': 2, 'W': 2, 'X': 1, 'Y': 2, 'Z': 1,
+};
+const FULL_HAND = 10;
+const C_MIN = 65;
+const C_MAX = 91;
+
 export const drawLetters = () => {
-  // Implement this method for wave 1
+  const hand = [];
+  const seen = {};
+
+  for (let i = 0; i < FULL_HAND; i++) {
+    const randomLetter = (() => {
+      let charCode = Math.floor(Math.random() * (C_MAX - C_MIN) + C_MIN);
+      return String.fromCharCode(charCode);
+    })();
+
+    if (seen[randomLetter] >= LETTER_DISTRIBUTION[randomLetter]) {
+      i--;
+      continue;
+    } else if (seen[randomLetter]) {
+      hand.push(randomLetter);
+      seen[randomLetter] += 1;
+    } else {
+      hand.push(randomLetter);
+      seen[randomLetter] = 1;
+    }
+  }
+
+  return hand;
+
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
